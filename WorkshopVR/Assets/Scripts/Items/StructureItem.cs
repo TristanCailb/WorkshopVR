d1ec4b1a25ccc0a8ebf65f2554862f2a585b82c9
TristanCailb <cailbourdint@gmail.com>
2019-05-28 12:41:15 +0200

    VR Grab

--- WorkshopVR/Assets/Scripts/Items/StructureItem.cs
+++ WorkshopVR/Assets/Scripts/Items/StructureItem.cs
@@ -3,7 +3,9 @@ public class SItem : UnityEngine.ScriptableObject //Structure de l'item
 {
     public string nom;              //Nom de l'item
     public string desciption;       //Description de l'item
+    public EItemEtat etat;          //Etat de l'item
     public EItemType type;          //Type d'item
+    public EItemAction action;      //Action de l'item sur PNJ
 }
 
 public enum EItemType //Types d'items
@@ -11,4 +13,17 @@ public enum EItemType //Types d'items
     ObjetDeMission,
     Consommable,
     Autre
+}
+
+public enum EItemEtat
+{
+    Normal,
+    Casse
+}
+
+public enum EItemAction //Action de l'item sur le PNJ quand il le reçoit
+{
+    Derange,
+    Blesse,
+    Tue
 }
