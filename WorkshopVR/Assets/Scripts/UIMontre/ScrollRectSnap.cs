2fc62859b9107ee2fef6e0fb36429f9952e051a6
TristanCailb <cailbourdint@gmail.com>
2019-05-30 15:05:09 +0200

    Debut Interface Montre

---
+++ WorkshopVR/Assets/Scripts/UIMontre/ScrollRectSnap.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollRectSnap : MonoBehaviour
+{
+    public RectTransform menusHolder;           //Panel qui contient les menus
+    public GameObject[] menus;                  //Tous les menus contenus dans le panel Content
+    public RectTransform centerToCompare;       //Centre du panel principal
+
+    private float[] distances;                  //Distances de chaque menu par rapport au centre
+    private bool dragging = false;              //Passe à true quand on drag le panel
+    private int menusDistance;                  //Distance entre chaque menu
+    private int minMenuIndex;                   //Index du menu le plus proche du centre
+
+    void Start()
+    {
+        int menuLength = menus.Length; //Nombre de menus dans l'array
+        distances = new float[menuLength]; //Créer un nouvel array de float de la taille du nombre de menus
+        // Calculer la distance entre 2 menus
+        menusDistance = (int)Mathf.Abs(menus[1].GetComponent<RectTransform>().anchoredPosition.x - menus[0].GetComponent<RectTransform>().anchoredPosition.x);
+    }
+
+    void Update()
+    {
+        for(int i = 0; i < menus.Length; i++)
+        {
+            distances[i] = Mathf.Abs(centerToCompare.transform.position.x - menus[i].transform.position.x);
+        }
+    }
+}
---
+++ WorkshopVR/Assets/Scripts/UIMontre/ToggleUIMode.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class ToggleUIMode : MonoBehaviour
+{
+    public bool isInUiMode;
+    public GameObject uiMontre; //Menu UI de la montre
+    public VRTK_Pointer rcPointer; //Element pointer du controller droit
+    public VRTK_BezierPointerRenderer rcBezierPointerRenderer; //Element Bezier renderer du controller droit
+    public VRTK_StraightPointerRenderer rcStraightPointerRenderer; //Element Straight pointer renderer du controller droit
+
+    public void ToggleMode()
+    {
+        isInUiMode = !isInUiMode;
+        if(isInUiMode)
+        {
+            rcPointer.pointerRenderer = rcStraightPointerRenderer;
+            rcPointer.enableTeleport = false;
+            uiMontre.SetActive(true);
+        }
+        else
+        {
+            rcPointer.pointerRenderer = rcBezierPointerRenderer;
+            rcPointer.enableTeleport = true;
+            uiMontre.SetActive(false);
+        }
+    }
+}
