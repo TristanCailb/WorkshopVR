8a0e77b2e4d8b0716984bb3a91bca576ad51bf2a
TristanCailb <cailbourdint@gmail.com>
2019-06-06 19:49:46 +0200

    Particules Objets Interactifs

---
+++ WorkshopVR/Assets/Scripts/Items/ObjetsInteractifs.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ObjetsInteractifs : MonoBehaviour
+{
+    public ParticleSystem particles;        //Particules à activer lors de l'interaction
+
+    public void StartParticles()
+    {
+        particles.Play();
+    }
+
+    public void StopParticles()
+    {
+        particles.Stop();
+    }
+}
